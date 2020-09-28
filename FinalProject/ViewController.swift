6574d66e95a04f57c9a96601d045c10f64602f87
Izacc Lucas <izacc.casey-lucas01@stclairconnect.ca>
2020-09-28 06:10:48 -0700

    Initial Commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Izacc Casey-Lucas on 9/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
