021b7fa70944dd548b3249cb5953b2b308806058
Izacc Lucas <izacc.casey-lucas01@stclairconnect.ca>
2020-09-29 07:02:49 -0700

    Changed dice selection to segue, was erring programmatically, simpler for future development

---
+++ FinalProject/Controller/DiceSelectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DiceSelectionViewController.swift
+//  FinalProject
+//
+//  Created by Izacc Casey-Lucas on 9/29/20.
+//
+
+import UIKit
+
+class DiceSelectionViewController: UIViewController {
+
+    
+    @IBOutlet weak var testLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FinalProject/Controller/RollViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Izacc Casey-Lucas on 9/28/20.
+//
+
+import UIKit
+
+class RollViewController: UIViewController {
+    //references the dice selection view
+    let DiceSelectionVc = DiceSelectionViewController()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
